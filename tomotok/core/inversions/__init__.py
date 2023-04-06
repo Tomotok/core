d80b6df1a4a9d25a398d750c66c95048381c9b82
Jakub Svoboda <svoboda@ipp.cas.cz>
2023-04-06 12:07:49 +0000

    Add fixed parameter Tikhonov scheme

--- tomotok/core/inversions/__init__.py
+++ tomotok/core/inversions/__init__.py
@@ -2,14 +2,16 @@
 #
 # Licensed under the EUPL-1.2 or later.
 """
-Inversion algorithms independent on the rest of the package
+Contains inversion algorithms. It is independent on the rest of the package.
 
 Currently implemented
  - Minimum Fisher Regularisation (MFR)
  - Linear Algebraic Methods (LAME)
  - Biorthogonal Basis Decomposition (BOB)
+ - Fixed parameter MFR
 """
 
 from .bob import Bob, SparseBob, SimpleBob, CholmodBob
 from .lame import SvdFastAlgebraic, GevFastAlgebraic
 from .mfr import Mfr, CholmodMfr
+from .fixed import Fixt, CholmodFixt
