39d88d2a9c4415c0a1e52c075d78d28004fc0793
svoboda <svoboda@ipp.cas.cz>
2025-05-14 14:58:35 +0200

    Updates of call of new mfr class

--- tomotok/core/inversions/minfish.py
+++ tomotok/core/inversions/minfish.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 
 from tomotok.tools.regularisation import regularisation_matrix
@@ -10,7 +11,35 @@ class Minimumfisher(object):
         self.solver = solver
         return
     
-    def __call__(self, data: np.ndarray, gmat, derivatives, errors, derivative_weights=None, mfi_num=3, **kwargs):
+    def __call__(
+        self, data: np.ndarray, gmat, derivatives, errors,
+        derivative_weights=None, mfi_num=3, compensare_negative_lines=False, 
+        **kwargs,
+    ) -> Tuple[np.ndarray, dict]:
+        """
+        Inversion using the Minimum Fisher Inversion method.
+
+        Parameters
+        ----------
+        data : np.ndarray
+        gmat : scipy.sparse.spmatrix
+        derivatives : list of scipy.sparse.spmatrix
+            list of derivative matrices with shape (#nodes, #nodes)
+        errors : np.ndarray
+        derivative_weights : list of floats or list of array-like, optional
+            anisotropy of derivatives, by default None
+        mfi_num : int, optional
+            number of minimum Fisher loops, by default 3
+        compensare_negative_lines : bool, optional
+            whether to balance the regularisation matrix, by default False
+
+        Returns
+        -------
+        np.ndarray
+            inversion result
+        dict
+            statistics of the inversion
+        """
         g = np.ones(gmat.shape[1])
         tmp_stats = []
 
@@ -18,7 +47,7 @@ class Minimumfisher(object):
             solver: Solver = self.solver()
             node_weights = 1 / g
             node_weights[g <= 0] = np.nanmax(node_weights)
-            regularisation = regularisation_matrix(derivatives, derivative_weights, node_weights)
+            regularisation = regularisation_matrix(derivatives, derivative_weights, node_weights, compensate_negative_lines=compensare_negative_lines)
             out, stats = solver(data, gmat, regularisation, errors, **kwargs)
             tmp_stats.append(stats)
             g = out
